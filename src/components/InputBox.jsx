e3a7342ae5b00c3c2c9399a878163e321ea1c69d
VanshSomal <vanshsomal20@gmail.com>
2026-07-13 23:49:34 +0530

    feat: react component InputBox added

---
+++ src/components/InputBox.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+function InputBox({
+    label,
+    amount,
+    onAmountChange,
+    onCurrencyChange,
+    currencyOptions=[],
+    selectCurrency="usd",
+    amountDisable=false,
+    currenyDisable=false,
+    className="",
+}){
+    return (
+        <div className={`bg-white p-3 rounded-lg text-sm flex ${className}`}>
+            <div className="w-1/2">
+                <label htmlFor={amountInputId} className="text-black/40 mb-2 inline-block">
+                    {label}
+                </label>
+                <input></input>
+            </div>
+        </div>
+    )
+}
